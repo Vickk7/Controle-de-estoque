90b790d31daf6ebc16a7ac2421d22ed01dac0ce3
Vickk7 <mavi_dos_santos@outlook.com>
2025-03-13 19:54:57 -0300

    Update and rename estoque.py to controle.py

---
+++ controle.py
@@ -0,0 +1,17 @@
+import customtkinter as ctk
+from tkinter import messagebox
+
+
+def cadastrar_produto():
+    pass
+
+def registrar_saida():
+    pass
+
+def registrar_entrada():
+    pass
+
+def remover_produto():
+    pass
+
+
